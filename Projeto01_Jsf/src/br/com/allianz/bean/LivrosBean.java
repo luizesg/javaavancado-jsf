7a5f34810da36909a07dea3ef1a5f0f3bb5c8c84
Luiz Eduardo <luizesg@hotmail.com>
2018-12-14 10:01:07 -0200

    novo projeto JSF

---
+++ Projeto01_Jsf/src/br/com/allianz/bean/LivrosBean.java
@@ -0,0 +1,49 @@
+package br.com.allianz.bean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.allianz.dao.LivrosDao;
+import br.com.allianz.models.Livro;
+
+@ManagedBean(name="beanLivro")
+@RequestScoped
+public class LivrosBean {
+
+	private Livro livro;
+
+	public LivrosBean() {
+		if(livro==null) {
+			livro = new Livro();
+		}
+	}
+	
+	
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+	
+	//method de ação (action) que quando executado retorna o
+//	redirecionamento conforme o resultado da execução
+	public String incluirLivro() {
+		try {
+			new LivrosDao().IncluirLivro(livro);
+			return "sucesso";
+		} catch (Exception e) {
+			return "erro";
+		}
+		
+	}
+	
+	//apresentar os livros cadastrados
+	 public List<Livro> getListaLivros() throws Exception{
+		 List<Livro> lista =  new LivrosDao().listarLivros();
+		 return lista;
+	 }
+}
